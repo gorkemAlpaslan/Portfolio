67de99fb423191fb51fd8b1372e1cf07467aef97
gorkemAlpaslan <gorkem.alpaslan98@gmail.com>
2022-10-28 23:56:59 +0300

    portfolio ui draft complated

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import styles from "../styles/Home.module.css";
+import ashes from "../public/Ashes.png";
+import Image from "next/image";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
