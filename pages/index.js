4e1dd4045852137b4456957a73ee9114c4586c0b
gorkemAlpaslan <gorkem.alpaslan98@gmail.com>
2022-11-03 19:08:35 +0300

    why ash page added

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,19 @@ import emailjs from "@emailjs/browser";
 export default function Home() {
   const [pageIsLoad, SetpageIsLoad] = useState("menu");
   const [ActivePage, SetActivePage] = useState();
+  const [isFormSubmit, SetIsFormSubmit] = useState({
+    isSubmit: false,
+    Message: "",
+  });
+  const [errorMessage, SetErrorMessage] = useState("");
   const ProjectsRef = useRef();
   const AboutRef = useRef();
   const ContactRef = useRef();
+
   const form = useRef();
+  const Email = useRef();
+  const Name = useRef();
+  const MessageContent = useRef();
 
   useEffect(() => {
     if (pageIsLoad === "menu") {
@@ -40,28 +49,54 @@ export default function Home() {
   const sendEmailHandler = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_gktrxa2",
-        "template_x9bpnrx",
-        form.current,
-        "Fj3Nx-NZLXGUPhan3"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    Email.current.value <= 0 && SetErrorMessage("Please Enter Valid E-mail");
+    Name.current.value <= 0 && SetErrorMessage("Please Enter Your Name");
+    MessageContent.current.value <= 0 &&
+      SetErrorMessage("Mail Must Be At Least 10 Character");
+
+    if (errorMessage === "") {
+      SetIsFormSubmit({
+        isSubmit: true,
+        Message: "Got your mail 😊",
+      });
+      emailjs
+        .sendForm(
+          "service_gktrxa2",
+          "template_x9bpnrx",
+          form.current,
+          "Fj3Nx-NZLXGUPhan3"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      setTimeout(() => {
+        SetIsFormSubmit({
+          isSubmit: false,
+          Message: "",
+        });
+      }, 3000);
+    } else if (errorMessage === "Please Enter Valid E-mail") {
+    }
   };
 
   return (
     <div>
       <div className={styles.container}>
         <div className={styles.MainTitle}>PORTFOLIO</div>
-        <div className={styles.SideLine}>why website concept is ash?</div>
+        <div
+          className={styles.SideLine}
+          onClick={() => {
+            SetActivePage("whyash");
+            SetpageIsLoad(undefined);
+          }}
+        >
+          why website concept is ash?
+        </div>
         <div className={styles.RefsWrapper}>
           <a href="https://www.instagram.com/gorkemddrn/" target="_blank">
             <InstagramIcon className={styles.RefImg} />
@@ -106,11 +141,12 @@ export default function Home() {
           </div>
         </div>
         <Image
+          alt="ashes"
           src={ashes}
           className={styles.ashes}
           onClick={AshesClickHandler}
         />
-        <Image src={gorkem} className={styles.NameAsImg}></Image>
+        <Image alt="Name" src={gorkem} className={styles.NameAsImg}></Image>
         <div className={styles.titles}>
           <div>
             <h2
@@ -162,6 +198,33 @@ export default function Home() {
               ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
               ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
               ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
+            </div>
+          </div>
+        )}
+        {ActivePage === "whyash" && (
+          <div className={styles.PageIsLoad}>
+            <div className={styles.PageTitle}>
+              <ArrowBackIosIcon
+                className={styles.BackIcon}
+                onClick={() => {
+                  SetActivePage();
+                  SetpageIsLoad("menu");
+                }}
+              ></ArrowBackIosIcon>
+              <div>Why website concept is ash?</div>
+            </div>
+            <div className={styles.AboutContent}>
+              Volcanic Ash scattered around as a result of the eruption of
+              volcanoes and many minerals come to the surface from the depths of
+              the ground. Thus, beneficial minerals and nutrients become
+              available to living species. Although it may seem harmful at
+              first, these ashes are very valuable for soil and plants. In the
+              long run, these ashes provide the creation of perhaps the most
+              productive agricultural areas on earth.
+            </div>
+            <div className={styles.AboutContent}>
+              The Concept here aims to convey my personal point of view in
+              addition to the projects that have actually been done.
             </div>
           </div>
         )}
@@ -249,23 +312,27 @@ export default function Home() {
               <div className={styles.UnderContactTitle}>
                 You can reach me easily
               </div>
+              {isFormSubmit === true && <div></div>}
               <div>
                 <div className={styles.PersonalInfoInput}>
                   <input
                     placeholder="Name"
                     type="text"
                     name="user_name"
+                    ref={Name}
                   ></input>
                   <input
                     placeholder="E-mail"
                     type="email"
                     name="user_email"
+                    ref={Email}
                   ></input>
                 </div>
                 <textarea
                   name="message"
                   placeholder="Enter your Message..."
                   className={styles.MessageInput}
+                  ref={MessageContent}
                 ></textarea>
               </div>
               <button type="submit" value="Send">
